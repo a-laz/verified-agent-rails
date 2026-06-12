db67aef8266df0cf87ff5be2fed553a3a9f6c670
Alex Lazarev <mortaleusmarketing@gmail.com>
2026-06-12 16:36:56 -0400

    chore: export contract ABIs from foundry artifacts via build script

---
+++ shared/src/abis/index.ts
@@ -0,0 +1,4 @@
+export { DelegationMirrorAbi } from "./DelegationMirror.js";
+export { GatedUSDAbi } from "./GatedUSD.js";
+export { MockYieldVaultAbi } from "./MockYieldVault.js";
+export { ServiceSinkAbi } from "./ServiceSink.js";
